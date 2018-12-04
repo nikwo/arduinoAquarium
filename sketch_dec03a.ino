33f7b32dd4e49a19c4e73b8f7f3874e2611a91ef
CCodingNovice <nikita.dubrovsky111@gmail.com>
2018-12-04 19:34:39 +0300

    oh_hello_there

--- sketch_dec03a.ino
+++ sketch_dec03a.ino
@@ -2,9 +2,9 @@
 
 
 iarduino_RTC time(RTC_DS1302,10,8,9);
-int xPin = A1;
+int xPin = A0;
 
-int yPin = A0;
+int yPin = A1;
 
 int buttonPin = 2;
 
@@ -18,35 +18,24 @@ void setup() {
     delay(300);
     Serial.begin(9600);
     time.begin(); //time initialization
-    //the time chip already set
+    //the time already set
 
     pinMode(xPin, INPUT);
 
     pinMode(yPin, INPUT);
 
-// активируем подтягивающий резистор на пине кнопки
-
 pinMode(buttonPin, INPUT_PULLUP);
 }
 void loop(){
-     //Serial.println(time.gettime("d m Y, H:i:s, D")); // showing real time
-     //Serial.println();
-     //delay(1000); 
+     Serial.println(time.gettime("d m Y, H:i:s, D")); // showing real time
+     Serial.println();
+     delay(1000); 
      xPosition = analogRead(xPin);
 
      yPosition = analogRead(yPin);
      buttonState = digitalRead(buttonPin);
 
-     Serial.print("X: ");
-
-     Serial.print(xPosition);
-     Serial.print(" | Y: ");
-
-     Serial.print(yPosition);
-
-     Serial.print(" | Button: ");
-
-     Serial.println(buttonState);
+     
 
      delay(100); // добавляем задержку между считыванием данных
 }
