7c5e57e8c691e4866c7943a7fa040484eac4c693
nikwo <36183206+CCodingNovice@users.noreply.github.com>
2018-12-24 13:31:36 +0300

    added control of lcd brightness with button

--- sketch_dec04a.ino
+++ sketch_dec04a.ino
@@ -2,18 +2,36 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
 iarduino_RTC t(RTC_DS1302,6,8,7);
-void setup(){
-Serial.begin(9600);
-t.begin();
-lcd.begin(16,2);
-lcd.clear();
+const int ButtonPin1 = 2;
+unsigned int timer;
+bool brightness;
 
+void setup(){
+  pinMode(ButtonPin1,INPUT);
+  Serial.begin(9600);
+  t.begin();
+  lcd.begin(16,2);
+  lcd.clear();
+  analogWrite(9,10);
+  brightness = false; //false - small brightness
+  timer = 0;
 }
 void loop(){
+  if (digitalRead(ButtonPin1)==HIGH && !brightness){
+    analogWrite(9,240);
+    brightness = true;
+  }
+  if (brightness){
+    ++timer;
+  }
+  if (timer >= 60 && brightness){
+    analogWrite(9,10);
+    brightness = false;
+    timer=0;
+  }
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(3,0);
   lcd.print(t.gettime("d-m-Y"));
   lcd.setCursor(3,1);
   lcd.print(t.gettime("H:i D"));
